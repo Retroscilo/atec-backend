2de023ca380f5d575781af5d4e64f976ee5d25ce
Loïc Torres <loic.torresg@gmail.com>
2021-08-20 11:29:19 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,14 @@
+/* module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '21e85407651496d95cb7e158c83794d1'),
+    },
+  },
+}); */
+
+module.exports = ({ env }) => ({
+  host: env('APP_HOST', '0.0.0.0'),
+  port: env.int('NODE_PORT', 1337),
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd5948b4b-62b1-47d8-a28a-dcc09f2c70c2'
+};

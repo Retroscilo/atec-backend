c2877388e56015963d15895034f531e8db00022a
Loïc Torres <loic.torresg@gmail.com>
2021-08-20 11:49:14 +0200

    fix(deploy): remove wysiwyg plugin

--- config/server.js
+++ config/server.js
@@ -1,4 +1,4 @@
-/* module.exports = ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
@@ -6,9 +6,9 @@
       secret: env('ADMIN_JWT_SECRET', '21e85407651496d95cb7e158c83794d1'),
     },
   },
-}); */
+});
 
-module.exports = ({ env }) => ({
+/* module.exports = ({ env }) => ({
   host: env('APP_HOST', '0.0.0.0'),
   port: env.int('NODE_PORT', 1337),
-});
+}); */
